9230b3badd44202fe5e99fcdc0120b332585d02d
des <destrat18@gmail.com>
2018-08-03 19:38:36 +0430

    complete city class

--- app/src/main/java/com/blit/blit/models/City.java
+++ app/src/main/java/com/blit/blit/models/City.java
@@ -1,4 +1,32 @@
 package com.blit.blit.models;
 
 public class City {
+
+    private String enName;
+    private String faName;
+    private String shortName;
+
+    public String getEnName() {
+        return enName;
+    }
+
+    public void setEnName(String enName) {
+        this.enName = enName;
+    }
+
+    public String getFaName() {
+        return faName;
+    }
+
+    public void setFaName(String faName) {
+        this.faName = faName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
 }
