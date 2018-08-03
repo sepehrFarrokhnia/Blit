398bad54ae22ceacf48ff0aa0771447c3d75bca7
Sepehr <sepehrfarrokhnia@gmail.com>
2018-08-03 19:39:35 +0430

    Ticket class updated

--- app/src/main/java/com/blit/blit/models/City.java
+++ app/src/main/java/com/blit/blit/models/City.java
@@ -1,4 +1,5 @@
 package com.blit.blit.models;
 
 public class City {
+
 }
--- app/src/main/java/com/blit/blit/models/Ticket.java
+++ app/src/main/java/com/blit/blit/models/Ticket.java
@@ -1,4 +1,59 @@
 package com.blit.blit.models;
 
+import java.util.Date;
+
 public class Ticket {
+    private long price;
+    private String detail;
+    private City destinationCity;
+    private City beginingCity;
+    private Date date;
+    private String company;
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public City getDestinationCity() {
+        return destinationCity;
+    }
+
+    public void setDestinationCity(City destinationCity) {
+        this.destinationCity = destinationCity;
+    }
+
+    public City getBeginingCity() {
+        return beginingCity;
+    }
+
+    public void setBeginingCity(City beginingCity) {
+        this.beginingCity = beginingCity;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
