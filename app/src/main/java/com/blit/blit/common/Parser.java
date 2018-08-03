c0a8121ad20490ca1a52d0f0e19b52c97e8faa99
destrat18 <destrat18@gmail.com>
2018-08-03 19:41:27 +0430

    add abstract methods in parser

--- app/src/main/java/com/blit/blit/common/Parser.java
+++ app/src/main/java/com/blit/blit/common/Parser.java
@@ -1,4 +1,12 @@
 package com.blit.blit.common;
 
+import com.blit.blit.models.City;
+import com.blit.blit.models.Ticket;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public abstract class Parser {
+    public abstract String getSearchPath(City c1, City c2, Date date);
+    public abstract ArrayList<Ticket> getTickets(City c1, City c2, Date date);
 }
