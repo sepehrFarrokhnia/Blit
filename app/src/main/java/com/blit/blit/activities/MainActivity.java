684ca1d9183440f5688355a5cd4ad1ae6f53a069
destrat18 <destrat18@gmail.com>
2018-08-03 19:47:25 +0430

    move activities

--- app/src/main/java/com/blit/blit/activities/MainActivity.java
+++ app/src/main/java/com/blit/blit/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.blit.blit;
+package com.blit.blit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.blit.blit.R;
 
 public class MainActivity extends AppCompatActivity {
 
