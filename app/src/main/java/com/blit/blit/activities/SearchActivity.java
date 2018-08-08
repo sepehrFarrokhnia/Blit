23d263db3f4131b3fd81d24835b56e2b8febb04b
destrat18 <destrat18@gmail.com>
2018-08-08 17:07:34 +0430

    add fonts add complete text to android

--- app/src/main/java/com/blit/blit/activities/SearchActivity.java
+++ app/src/main/java/com/blit/blit/activities/SearchActivity.java
@@ -12,12 +12,12 @@ import java.util.ArrayList;
 public class SearchActivity extends AppCompatActivity {
     public static ArrayList<City> cities = new ArrayList<>();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
     public void loadCities(){
         City mashhad = new City("MHD","فارسی","Mashhad");
         City tehran = new City("THR","تهران","Tehran");
